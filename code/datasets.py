894dbb4c32b5c349d1e236ad6a731690a20e3228
Kenneth <43699191+WrightKD@users.noreply.github.com>
2023-04-01 19:09:02 +0200

    Update dir path

--- code/datasets.py
+++ code/datasets.py
@@ -6,17 +6,17 @@ from torchvision import transforms
 class MnistDataset(torch.utils.data.Dataset):
     def __init__(self, training=True, transform=None):
         if training==True:
-            f = open('../data/MNIST/raw/train-images-idx3-ubyte', 'rb')
+            f = open('/kaggle/working/MnistSimpleCNN/data/MNIST/raw/train-images-idx3-ubyte', 'rb')
             xs = np.array(np.frombuffer(f.read(), np.uint8, offset=16))
             f.close()
-            f = open('../data/MNIST/raw/train-labels-idx1-ubyte', 'rb')
+            f = open('/kaggle/working/MnistSimpleCNN/data/MNIST/raw/train-labels-idx1-ubyte', 'rb')
             ys = np.array(np.frombuffer(f.read(), np.uint8, offset=8))
             f.close()
         else:
-            f = open('../data/MNIST/raw/t10k-images-idx3-ubyte', 'rb')
+            f = open('/kaggle/working/MnistSimpleCNN/data/MNIST/raw/t10k-images-idx3-ubyte', 'rb')
             xs = np.array(np.frombuffer(f.read(), np.uint8, offset=16))
             f.close()
-            f = open('../data/MNIST/raw/t10k-labels-idx1-ubyte', 'rb')
+            f = open('/kaggle/working/MnistSimpleCNN/data/MNIST/raw/t10k-labels-idx1-ubyte', 'rb')
             ys = np.array(np.frombuffer(f.read(), np.uint8, offset=8))
             f.close()
         xs = np.reshape(xs, (-1, 28, 28, 1)).astype(np.float32)
