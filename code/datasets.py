a093f9373a58f6117695b1b4fd5b8937081480e3
Kenneth <43699191+WrightKD@users.noreply.github.com>
2023-04-01 19:25:48 +0200

    try different data source

--- code/datasets.py
+++ code/datasets.py
@@ -21,8 +21,10 @@ class MnistDataset(torch.utils.data.Dataset):
             f.close()
         xs = np.reshape(xs, (-1, 28, 28, 1)).astype(np.float32)
         ys = ys.astype(np.int)
-        self.x_data = xs
-        self.y_data = ys
+        
+        (x_train, y_train) = mnist_extract_data("/kaggle/input/bbd-digit-recognizer/train.csv")
+        self.x_data = x_train
+        self.y_data = y_train
         self.transform = transform
 
     def __len__(self):
